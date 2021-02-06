4afeedebe3e7abfb714e3d8f7dbe1213442bf906
Veeranki Naveen <61687750+Naveen-Automation@users.noreply.github.com>
2021-02-06 11:22:18 +0000

    First Commit

---
+++ FeatureFiles/Login.feature
@@ -0,0 +1,8 @@
+﻿Feature: Login
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Launch Browser
+	Given I launch browser "chrome" browser
+	#When I search text "selenium"
+	#Then I should see selenium results
---
+++ StepDefinitions/CommonStepDefinition.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;              //IWebdriver definition present here
+using OpenQA.Selenium.Chrome;       // ChromeDriver class presence
+using OpenQA.Selenium.Firefox;      // Firefox class presence
+using System;
+using TechTalk.SpecFlow;
+
+namespace Selenium_BDD_Framework
+{
+    [Binding]
+    public class CommonStepDefinition
+    {
+        public IWebDriver driver;
+
+        [Given(@"I launch browser ""(.*)"" browser")]
+        public void GivenILaunchBrowserBrowser(string browserType)
+        {
+            switch (browserType)
+            {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+            }
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+          
+        }
+
+        [When(@"I search text ""(.*)""")]
+        public void WhenISearchText(string searchText)
+        {
+           
+        }
+
+
+
+        [Then(@"I should see selenium results")]
+        public void ThenIShouldSeeSeleniumResults()
+        {
+            
+        }
+
+    }
+}
