cffbd89a30bff1084dd9a42882669e01be9eaf1a
Veeranki Naveen <61687750+Naveen-Automation@users.noreply.github.com>
2021-02-07 01:37:48 +0000

    Updated Jenkins File

--- StepDefinitions/CommonStepDefinition.cs
+++ StepDefinitions/CommonStepDefinition.cs
@@ -23,7 +23,7 @@ namespace Selenium_BDD_Framework
                     driver = new FirefoxDriver();
                     break;
             }
-            driver.Navigate().GoToUrl("http://www.facebook.com");
+            driver.Navigate().GoToUrl("http://www.yahoo.com");
 
           
         }
